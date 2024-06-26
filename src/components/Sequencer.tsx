b915a1e2cc134ab63d85d138380283b46106b1a0
Edward Horsey <edwardhorsey@protonmail.com>
2024-06-26 21:15:12 +0100

    Add StartStopButton

--- src/components/Sequencer.tsx
+++ src/components/Sequencer.tsx
@@ -1,24 +1,15 @@
 import Tracks from '@components/Tracks';
-import useTrackStore from '@stores/useTrackStore';
-import shallow from 'zustand/shallow';
+import StarStopButton from './StartStopButton';
 
 function Sequencer(): JSX.Element {
-    const [isPlaying, start, stop] = useTrackStore((state) => [state.isPlaying, state.start, state.stop], shallow);
-
     return (
-        <div>
+        <section>
             <div className="w-full flex mb-2 pt-6 pl-4">
-                <button
-                    type="button"
-                    className="w-28 text-center py-2 px-4 border bg-white border-black rounded-md"
-                    onClick={isPlaying ? stop : start}
-                >
-                    {isPlaying ? 'Stop' : 'Start'}
-                </button>
+                <StarStopButton />
             </div>
 
             <Tracks />
-        </div>
+        </section>
     );
 }
 
---
+++ src/components/StartStopButton.tsx
@@ -0,0 +1,16 @@
+import useTrackStore from '@stores/useTrackStore';
+import shallow from 'zustand/shallow';
+
+export default function StarStopButton() {
+    const [isPlaying, start, stop] = useTrackStore((state) => [state.isPlaying, state.start, state.stop], shallow);
+
+    return (
+        <button
+            type="button"
+            className="w-28 text-center py-2 px-4 border bg-white border-black rounded-md"
+            onClick={isPlaying ? stop : start}
+        >
+            {isPlaying ? 'Stop' : 'Start'}
+        </button>
+    );
+}
