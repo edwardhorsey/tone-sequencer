d674c4f608fdf38a970ce58369c999bc91453aa3
Edward Horsey <edwardhorsey@protonmail.com>
2024-06-27 08:02:05 +0100

    Fix pitches order

--- src/lib/trackHelpers.ts
+++ src/lib/trackHelpers.ts
@@ -1,17 +1,20 @@
 import { Loop } from '@lib/types/sequencer';
 
-const pitches = ['C', 'C#', 'D', 'Eb', 'E', 'F', 'Gb', 'G', 'Ab', 'A', 'Bb', 'B'];
+const pitches = ['B', 'A#', 'A', 'G#', 'G', 'F#', 'F', 'E', 'D#', 'D', 'C#', 'C'];
 
 export const pitchesByOctaves: string[] = ['5', '4', '3', '2'].reduce((acc: string[], octave) => {
-    return acc.concat(pitches.map((pitch) => `${pitch}${octave}`));
+    acc.push(...pitches.map((pitch) => `${pitch}${octave}`));
+
+    return acc;
 }, []);
 
 export function generateRandomLoop(octave = 3) {
+    // Length of loop
     const division = Math.round(Math.random() * 15) + 2;
     const loop: Loop = [];
 
     for (let i = 0; i < division; i += 1) {
-        // 40% chance of a note
+        // 40% chance
         if (Math.random() > 0.6) {
             const pitch = pitches[Math.round(Math.random() * (pitches.length - 1))] + String(octave);
 
